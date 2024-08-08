98c5cbffa08986254a8be8ce556cc90283b477f1
Adrian Sanchez <electrocamo@gmail.com>
2024-08-08 10:35:17 -0500

    Se arregla error de conexion en script

--- scripts/monitoring.js
+++ scripts/monitoring.js
@@ -5,20 +5,23 @@ import { serviceModel } from "../src/models/service.model.js";
 import { connectDb } from "../src/database.js";
 
 export const monitorServices = async () => {
-  const db = await connectDb()
   try {
+    const db = await connectDb()
     const [services] = await db.query("SELECT * FROM services;")
+    db.end()
 
-    services.map(async (item) =>{
+    services.map(async (item) => {
       try {
         const response = await axios.get(`http://${item.ip}`);
+        console.log("name: ", item.name)
         const isActive = response.status === 200;
         if (item.status !== isActive) {
+          const db = await connectDb()
           await db.query("UPDATE services SET status = ? where id = ?;", [isActive ? 1 : 0, item.id])
+          db.end()
 
           console.log(
-            `Status updated for service ${item.name}: ${
-              isActive ? "Asset" : "Idle"
+            `Status updated for service ${item.name}: ${isActive ? "Asset" : "Idle"
             }`
           );
         }
@@ -26,13 +29,18 @@ export const monitorServices = async () => {
         console.error(
           `Error checking status for service ${item.name}: ${error.message}`
         );
+        const db = await connectDb()
+        await db.query("UPDATE services SET status = ? where id = ?;", [0, item.id])
+        db.end()
+
       }
     });
+    db.end()
+
     console.log("Services monitored and updated correctly.");
   } catch (error) {
     console.error("Error when monitoring services:", error.message);
   } finally {
-    db.end()
     console.error("closing bd connection");
   }
 };
